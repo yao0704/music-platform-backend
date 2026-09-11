cf21a75428f36750aca4192314a00fa50e37ff74
yao0704 <1450348907@qq.com>
2026-09-11 20:28:48 +0800

    初始化SpringBoot项目骨架

---
+++ src/main/java/com/music/musicplatform/MusicPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.music.musicplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/music/musicplatform/MusicPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.music.musicplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
